7246e39a07308a646c842b2c07865be8e2643fb5
Sahil Karale <karalesahil2002@gmail.com>
2025-07-27 22:17:52 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predicting Swiggy delivery time using a machine learning model to enhance customer satisfaction and optimize logistics.',
+    author='Sahil',
+    license='',
+)
